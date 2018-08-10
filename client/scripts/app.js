01965deb8375ff03615512d02cb26d666ba82312
rebz135 <rebz135@gmail.com>
2018-08-10 14:48:59 -0700

    Init app structure

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,34 @@
 // YOUR CODE HERE:
+
+class App {
+  constructor() {
+    //DO SOMETHING;
+  }
+
+  init() {}
+
+  send() {
+    $.ajax({
+      url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+
+  fetch() {}
+
+  clearMessages() {}
+
+  renderMessages() {}
+
+  renderRoom() {}
+}
+
+let app = new App();
