51d6e07d6633dae6001790dd2c613f596623cfd0
rebz135 <rebz135@gmail.com>
2018-08-10 20:10:15 -0700

    Add hard refresh capability

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -40,19 +40,6 @@ class App {
     $('#chats').empty();
   }
 
-  renderMessage(message) {
-    let div = document.createElement('div');
-    $(div).text(message); //prevent XSS attacks
-    $('#chats').append(div);
-  }
-
-  renderRoom(room) {
-    let div = document.createElement('div');
-    $(div).text(room); //prevent XSS attacks
-    $('#roomSelect').append(div);
-    //TBD needs onclick action to move to room page
-  }
-
   renderChat(data) {
     let div = document.createElement('div');
     let text = document.createElement('a');
@@ -64,7 +51,7 @@ class App {
     $(div).append(space);
     $(div).append(author);
     $(div).append(roomstamp);
-    $(div).addClass('chatcontainer');
+    $(div).addClass('chatContainer');
     $(text).addClass('chats');
     $(author).addClass('user');
     $(roomstamp).addClass('room');
@@ -73,8 +60,8 @@ class App {
     } else {
       $(text).text('TEXT NOT FOUND');
     }
-    if (data.user) {
-      $(author).text(data.user); //prevent XSS attacks
+    if (data.username) {
+      $(author).text(data.username); //prevent XSS attacks
     } else {
       $(author).text('AUTHOR NOT FOUND');
     }
@@ -88,61 +75,45 @@ class App {
   }
 }
 
-let app = new App();
+$(document).ready(() => {
+  let app = new App();
 
-let renderAllRooms = function(data) {
-  let rooms = {};
-  for (let i = 0; i < data.results.length; i++) {
-    if (data.results[i].roomname) {
-      rooms[data.results[i].roomname] = i;
+  let renderChatFeed = function(data) {
+    for (let i = 0; i < data.results.length; i++) {
+      app.renderChat(data.results[i]);
     }
-  }
-  for (key in rooms) {
-    if (key !== '') {
-      app.renderRoom(key);
-    }
-  }
-};
+  };
 
-let renderAllMessages = function(data) {
-  let message = {};
-  for (let i = 0; i < data.results.length; i++) {
-    if (data.results[i].text) {
-      message[data.results[i].text] = i;
-    }
-  }
-  for (key in message) {
-    if (key !== '') {
-      app.renderMessage(key);
-    }
-  }
-};
+  //initialize renderings
+  app.fetch(renderChatFeed);
 
-let renderChatFeed = function(data) {
-  console.log(data);
-  for (let i = 0; i < data.results.length; i++) {
-    app.renderChat(data.results[i]);
-  }
-};
+  let refreshChatFeed = function() {
+    $('.chatContainer').remove();
+    app.fetch(renderChatFeed);
+  };
 
-//initialize renderings
-app.fetch(renderChatFeed);
-app.fetch(renderAllRooms);
+  setInterval(refreshChatFeed, 50000);
 
-//jQuery
+  $('.btn').click(function() {
+    let message = {
+      username: $('input[name=inputUser]').val(),
+      text: $('input[name=inputRoom]').val(),
+      roomname: $('input[name=inputMessage]').val()
+    };
+    app.send(message);
+    refreshChatFeed();
+  });
+});
+
+//OBJECT FROM PARSE
+// createdAt: "2018-08-11T01:55:27.038Z"
+// objectId:"aRFqlHTUkF"
+// roomname:"Main"
+// text:"GROUND CONTROL TO MAJOR TOM Fri Aug 10 2018 18:55:27 GMT-0700 (Pacific Daylight Time)"
+// updatedAt:"2018-08-11T01:55:27.038Z"
+// username:"AVH"
 
 //TODO
-//Refresh displayed messages
-//Allows users to select username and send messages
-//Create rooms
 //Enter existing rooms
 //Allow users to befriend other users by clicking on their username
 //Display messages sent by friends in bold
-
-var message = {
-  username: 'AVH',
-  text: 'testing for seeing this message',
-  roomname: 'Main'
-};
-
-app.send(message);
